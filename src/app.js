c7b9cf4e48b84c90300b13460cb78ecfe8c89517
Jose Rodriguez <masullo.joseluis@gmail.com>
2026-06-17 18:45:37 +0000

    juego ok

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
 //write your code here
-console.log("Hello Rigo from the console!");
+const opciones = ["rock", "paper", "scissors", "lizard", "spock"]
 
+const jugar = (usuarioElec) => {
+    const opciones = ["rock", "paper", "scissors", "lizard", "spock"];
+
+    let computerElec = opciones[Math.floor(Math.random() * opciones.length)];
+
+    const rules = {
+        rock: ["scissors", "lizard"],
+        paper: ["rock", "spock"],
+        scissors: ["paper", "lizard"],
+        lizard: ["paper", "spock"],
+        spock: ["rock", "scissors"]
+    };
+
+    if (usuarioElec === computerElec) {
+        return "Empate :/";
+    }
+    if (rules[usuarioElec].includes(computerElec)) {
+        return "Has Ganado!!";
+    }
+    return "Perdiste :(";
+};
+
+console.log(jugar("rock"));
